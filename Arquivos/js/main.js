bdd738a7ff56ee3a5e42ea99a66d9dad400d0eca
A-LucasCastro <luquinhascastro1@hotmail.com>
2022-11-02 15:15:48 -0300

    Quase pronto

--- Arquivos/js/main.js
+++ Arquivos/js/main.js
@@ -2,28 +2,74 @@ const inputTarefa = document.querySelector('.input-tarefas')
 const btnTarefa = document.querySelector('.btn-tarefa')
 const tarefas = document.querySelector('.tarefas')
 
+inputTarefa.addEventListener('click', function() {
+    const input = document.querySelector('.input-tarefas')
+    input.classList.remove('erro')
+})
+
+function criaDiv() {
+    const div = document.createElement('div')
+    return div
+}
+
+function criaDivPai() {
+    const divPai = document.createElement('div')
+    return divPai
+}
+
 function criaLi() {
     const li = document.createElement('li')
     return li
 }
 
-function criaBotaoApagar(li) {
-    const botaoApagar = document.createElement('i')
-    botaoApagar.classList.add('fa-regular')
-    botaoApagar.classList.add('fa-trash-can')
-    li.appendChild(botaoApagar)
+function limpaInput() {
+    inputTarefa.value = '';
+    inputTarefa.focus();
+}
+
+function criaLixo(div) {
+    const lixeiro = document.createElement('i')
+    lixeiro.classList.add('far')
+    lixeiro.classList.add('fa-trash-alt')
+    div.classList.add('lixo')
+    div.appendChild(lixeiro)
+}
+
+function criaBotaoApagar(div) {
+    div.classList.add('apagar')
 }
 
 function criaTarefa(textoInput) {
     const li = criaLi()
+    const div = criaDiv()
+    const divPai = criaDivPai()
     li.innerHTML = textoInput
-    tarefas.appendChild(li)
-    criaBotaoApagar(li)
+    tarefas.appendChild(divPai)
+    divPai.appendChild(li)
+    divPai.appendChild(div)
+    criaBotaoApagar(div)
+    criaLixo(div)
+    limpaInput()
 }
 
+document.addEventListener('click', function(e) {
+    const el = e.target
+
+    console.log(el)
+
+    if (el.classList.contains('apagar')) {
+        el.parentElement.remove()
+    }
+})
+
 btnTarefa.addEventListener('click', function() {
-    if (!inputTarefa.value) return
+    if (!inputTarefa.value) {
+        const input = document.querySelector('.input-tarefas')
+        input.classList.add('erro')
+        return
+    }
+
     criaTarefa(inputTarefa.value)
 })
 
-//<i class="fa-solid fa-trash-can"></i>
+//<i class="far fa-trash-alt"></i>
